abba3e742cb01c0805f26f4563e8a3d9d34facb7
gabriel.itagyba <gabriel.itagyba@7comm.com.br>
2024-09-13 03:54:57 -0300

    remove unused files

--- clear_data_mysql.sh
+++
@@ -1,4 +0,0 @@
-for i in {1..8}; do
-  docker exec -i mysql-debezium-project-mysql${i}-1 mysql -uuser${i} -ppass${i} -e "DROP DATABASE db${i}; CREATE DATABASE db${i};"
-  echo "Cleared content for mysql${i}"
-done
